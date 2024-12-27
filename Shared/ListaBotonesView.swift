49c9af61e90ed89b6d9fd9980728dbb66a699a70
Rene <rene.soto01@iest.edu.mx>
2024-12-26 18:50:31 -0600

    Se agrego una tarjeta de credito con efecto de hacerse mas grande o pequena dependiendo el clic

--- Shared/ListaBotonesView.swift
+++ Shared/ListaBotonesView.swift
@@ -8,19 +8,44 @@
 import SwiftUI
 
 struct ListaBotonesView: View {
+    
+    @State var aumento: CGFloat = 1.0
+    
+    func efectoAumentoTamano(){
+        withAnimation( .spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0.5)
+        ){
+            if(aumento == 1.0) {
+                aumento = aumento + 0.2
+            }else {
+                aumento = 1.0
+            }
+        }
+    }
+    
     var body: some View {
-        ScrollView(.horizontal){
-            HStack{
-                BotonesView()
-                BotonesView()
-                BotonesView()
-                BotonesView()
-                BotonesView()
-                BotonesView()
-                BotonesView()
-                BotonesView()
-                BotonesView()
-                
+        VStack{
+            
+            
+            CreditCardView(bank: "Stori", number: "1234 5678 9101 2134")
+                .scaleEffect(aumento)
+                .onTapGesture {
+                    efectoAumentoTamano()
+                }
+            
+            
+            ScrollView(.horizontal){
+                HStack{
+                    BotonesView()
+                    BotonesView()
+                    BotonesView()
+                    BotonesView()
+                    BotonesView()
+                    BotonesView()
+                    BotonesView()
+                    BotonesView()
+                    BotonesView()
+                    
+                }
             }
         }
     }
