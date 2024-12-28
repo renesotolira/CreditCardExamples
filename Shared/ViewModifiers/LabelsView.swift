5dd7db919ee9abe52527bf81ddb4c563ec2d0048
Rene <rene.soto01@iest.edu.mx>
2024-12-27 18:34:16 -0600

    Diseno de label con carro

--- Shared/ViewModifiers/LabelsView.swift
+++ Shared/ViewModifiers/LabelsView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct LabelsView: View {
     var body: some View {
-        Label("Carro", systemImage: "car")
+        
+     
+            Label("Carro", systemImage: "car")
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.white)
+                .cornerRadius(12)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke( Color(
+                            red: 1/255, green: 130/255, blue: 230/255 , opacity: 0.2
+                        ) , lineWidth: 2 )//finaliza stroke
+                        .padding()
+                )
     }
 }
 
