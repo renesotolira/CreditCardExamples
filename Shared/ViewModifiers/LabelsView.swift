c9e717cfdb04056e844c256e0b8e3c23e044ce6d
Rene <rene.soto01@iest.edu.mx>
2024-12-27 18:55:31 -0600

    Diseno del carro y viewmodifiers redondeado gris pero con color cambiable terminado.

--- Shared/ViewModifiers/LabelsView.swift
+++ Shared/ViewModifiers/LabelsView.swift
@@ -10,19 +10,16 @@ import SwiftUI
 struct LabelsView: View {
     var body: some View {
         
-     
-            Label("Carro", systemImage: "car")
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(Color.white)
-                .cornerRadius(12)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke( Color(
-                            red: 1/255, green: 130/255, blue: 230/255 , opacity: 0.2
-                        ) , lineWidth: 2 )//finaliza stroke
-                        .padding()
-                )
+        VStack{
+        
+            Label("Carros", systemImage: "car")
+                .modifier( RoundGrayViewModifer()  )
+            
+            Text("hola")
+                .modifier( RoundGrayViewModifer( strokeWidth: 8 , color: Color.red)  )
+            
+        }
+                
     }
 }
 
